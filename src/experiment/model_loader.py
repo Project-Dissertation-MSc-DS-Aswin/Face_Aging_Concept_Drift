c9ad2b9e77c0b4540e6b10e84aaad1daf65e54b2
Aswin Vijayakumar <aswinkvj@gmail.com>
2022-08-11 07:59:48 +0100

    FaceNet without aging (calculation of mahalanobis distances)

--- src/experiment/model_loader.py
+++ src/experiment/model_loader.py
@@ -1,10 +1,15 @@
 import sys
 import os
+from matplotlib import image
 import tensorflow as tf
+import keras
+from tensorflow import keras
 from tensorflow.keras.models import load_model, Model
 from keras.preprocessing.image import ImageDataGenerator
 import cv2
+from experiment.yunet import YuNet
 from pipeline.context import Constants
+from tensorflow.keras import backend as K
 constants = Constants()
 
 sys.path.append(constants.LATS_REPO)
@@ -28,11 +33,14 @@ def get_augmented_datasets():
 class KerasModelLoader:
   dimensions = 128
   
-  def __init__(self, logger, model_path, input_shape=None):
+  def __init__(self, logger, model_path, input_shape=None, image_data_format='channels_last'):
     self.logger = logger
     self.model_path = model_path
     self.type = 'keras'
-    (b, input_w, input_h, n_channels) = input_shape
+    if image_data_format == 'channels_last':
+      (b, input_w, input_h, n_channels) = input_shape
+    elif image_data_format == 'channels_first':
+      (b, n_channels, input_w, input_h) = input_shape
     self.input_w = input_w
     self.input_h = input_h
     self.input_shape = input_shape
@@ -59,6 +67,60 @@ class KerasModelLoader:
 class FaceNetKerasModelLoader(KerasModelLoader):
   pass
 
+from keras import regularizers
+
+class ArcFace(keras.layers.Layer):
+    def __init__(self, n_classes=10, s=30.0, m=0.50, regularizer=regularizers.l2(0.0001), **kwargs):
+        super(ArcFace, self).__init__(**kwargs)
+        self.n_classes = n_classes
+        self.s = s
+        self.m = m
+        self.regularizer = regularizers.get(regularizer)
+
+    def build(self, input_shape):
+        super(ArcFace, self).build(input_shape[0])
+        self.W = self.add_weight(name='W',
+                                shape=(input_shape[0][-1], self.n_classes),
+                                initializer='glorot_uniform',
+                                trainable=True,
+                                regularizer=self.regularizer)
+
+    def call(self, inputs):
+        x, y = inputs
+        c = K.shape(x)[-1]
+        # normalize feature
+        x = tf.nn.l2_normalize(x, axis=1)
+        # normalize weights
+        W = tf.nn.l2_normalize(self.W, axis=0)
+        # dot product
+        logits = x @ W
+        # add margin
+        # clip logits to prevent zero division when backward
+        theta = tf.acos(K.clip(logits, -1.0 + K.epsilon(), 1.0 - K.epsilon()))
+        target_logits = tf.cos(theta + self.m)
+        # sin = tf.sqrt(1 - logits**2)
+        # cos_m = tf.cos(logits)
+        # sin_m = tf.sin(logits)
+        # target_logits = logits * cos_m - sin * sin_m
+        #
+        logits = logits * (1 - y) + target_logits * y
+        # feature re-scale
+        logits *= self.s
+
+        return logits
+
+    def compute_output_shape(self, input_shape):
+        return (None, self.n_classes)
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({
+            "s": self.s,
+            "m": self.m,
+            "n_classes": self.n_classes
+        })
+        return config
+
 class FaceRecognitionBaselineKerasModelLoader(KerasModelLoader):
   dimensions = 717
   
@@ -67,8 +129,54 @@ class FaceRecognitionBaselineKerasModelLoader(KerasModelLoader):
   """
   def load_model(self):
     print(os.path.isfile(self.model_path))
-    self.model = load_model(self.model_path, compile=False)
+    self.model = load_model(self.model_path, compile=False, custom_objects={"ArcFace": ArcFace})
     self.model = Model(inputs=self.model.inputs, outputs=self.model.layers[-6].output)
     self.logger.log({
       "keras_model_summary": self.model.summary()
     })
+
+
+class MNIST2Epochs(KerasModelLoader):
+  pass
+
+class MNIST5Epochs(KerasModelLoader):
+  pass
+
+class MNIST10Epochs(KerasModelLoader):
+  pass
+
+class YuNetModelLoader:
+  
+  def __init__(self, logger, model_path, conf_threshold, nms_threshold, backend, target, top_k, input_shape=[1, 96, 96, 1]):
+    self.input_shape = input_shape
+    (b, input_h, input_w, n_channels) = input_shape
+    self.input_w = input_w
+    self.input_h = input_h
+    self.logger = logger
+    self.model_path = model_path
+    self.conf_threshold = conf_threshold
+    self.nms_threshold = nms_threshold
+    self.backend = backend
+    self.target = target
+    self.top_k = top_k
+    self.load_model()
+  
+  def load_model(self):
+    self.detector = YuNet(modelPath=self.model_path,
+                  inputSize=(self.input_h, self.input_w),
+                  confThreshold=self.conf_threshold,
+                  nmsThreshold=self.nms_threshold,
+                  topK=self.top_k,
+                  backendId=self.backend,
+                  targetId=self.target)
+    
+  def resize(self, data):
+    return self.detector.setInputSize((self.input_h, self.input_w))
+  
+  """
+  data: Image
+  """
+  def infer(self, data):
+    return self.detector.infer(data)
+  
+  
--- src/pipeline/facenet_without_aging.py
+++ src/pipeline/facenet_without_aging.py
@@ -3,12 +3,15 @@ import tensorflow as tf
 from dataloaders import DataGenerator
 from datasets import CACD2000Dataset, FGNETDataset, AgeDBDataset
 from experiment.facenet_without_aging import FaceNetWithoutAgingExperiment
+from experiment.face_classification_by_images import FaceClassificationByImages
 from experiment.model_loader import FaceNetKerasModelLoader, FaceRecognitionBaselineKerasModelLoader
 import whylogs
-from experiment.model_loader import get_augmented_datasets, preprocess_data_facenet_without_aging
+from experiment.model_loader import get_augmented_datasets, preprocess_data_facenet_without_aging, YuNetModelLoader
 import numpy as np
+import pandas as pd
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+from copy import copy
 import os
 import sys
 import re
@@ -17,6 +20,7 @@ import argparse
 from pathlib import Path
 import logging
 import mlflow
+import cv2
 from collections import Counter
 
 """
@@ -29,16 +33,26 @@ args = Args({})
 Arguments to the pipeline
 """
 args.dataset = os.environ.get('dataset', 'agedb')
+args.source_type = os.environ.get('source_type', 'ordered_metadata')
+args.model_detect = os.environ.get('model_detect', 'YuNet_onnx')
+args.model_path_detect = os.environ.get('model_path_detect', '../models/face_detection_yunet_2022mar.onnx')
 args.model = os.environ.get('model', 'FaceNetKeras')
-args.model_path = os.environ.get('model_path', 'facenet_keras.h5')
+args.model_path = os.environ.get('model_path', '../models/facenet_keras.h5')
 args.data_dir = os.environ.get('data_dir', constants.AGEDB_DATADIR)
 args.batch_size = int(os.environ.get('batch_size', 128))
 args.preprocess_whiten = int(os.environ.get('preprocess_whiten', 1))
 args.data_collection_pkl = os.environ.get('data_collection_pkl', constants.AGEDB_FACENET_INFERENCES)
+args.drift_source_filename = os.environ.get('drift_source_filename', constants.AGEDB_DRIFT_SOURCE_FILENAME)
 args.metadata = os.environ.get('metadata', constants.AGEDB_METADATA)
 args.logger_name = os.environ.get('logger_name', 'facenet_without_aging')
 args.no_of_samples = int(os.environ.get('no_of_samples', 2248))
 args.colormode = os.environ.get('colormode', 'rgb')
+args.conf_threshold = os.environ.get("conf_threshold", 0.9)
+args.nms_threshold = os.environ.get("nms_threshold", 0.9)
+args.backend = os.environ.get('backend', cv2.dnn.DNN_BACKEND_OPENCV)
+args.target = os.environ.get('target', cv2.dnn.DNN_TARGET_CPU)
+args.top_k = os.environ.get('top_k', 5000)
+args.unique_name_count = os.environ.get('unique_name_count', 40)
 args.experiment_id = os.environ.get('experiment_id', 1)
 args.input_shape = os.environ.get('input_shape', (-1,160,160,3))
 
@@ -57,9 +71,15 @@ for parameter in parameters:
     exec("args." + parameter)
     
 args.experiment_id = int(args.experiment_id)
+args.target = int(args.target)
+args.backend = int(args.backend)
+args.nms_threshold = float(args.nms_threshold)
+args.top_k = int(args.top_k)
+args.conf_threshold = float(args.conf_threshold)
 args.batch_size = int(args.batch_size)
 args.preprocess_whiten = int(args.preprocess_whiten)
 args.no_of_samples = int(args.no_of_samples)
+args.unique_name_count = int(args.unique_name_count)
 if type(args.input_shape) == str:
     input_shape = args.input_shape.replace('(','').replace(')','').split(",")
     args.input_shape = tuple([int(s) for s in input_shape if s.strip() != '' or s.strip() != ','])
@@ -69,17 +89,53 @@ def get_reduced_metadata(args, dataset):
   if args.dataset == "fgnet":
     return dataset.metadata
   elif args.dataset == "agedb":
-    np.random.seed(1000)
-    names = np.unique(dataset.metadata['name'])[:435]
-    idx = [dataset.metadata['name'] == name for name in names]
-    result_idx = [False] * len(dataset.metadata)
-    for i in idx:
-        result_idx = np.logical_or(result_idx, i)
-
-    return dataset.metadata.loc[result_idx].sort_values(by=['name', 'age'])
+    if args.source_type == 'file':
+      np.random.seed(1000)
+      filenames = pd.read_csv(args.drift_source_filename)
+      idx = [dataset.metadata['filename'] == filename for filename in filenames['filename']]
+      result_idx = [False]*len(dataset.metadata)
+      for i in idx:
+          result_idx = np.logical_or(result_idx, i)
+      
+      return dataset.metadata.loc[result_idx].reset_index()
+    elif args.source_type == "ordered_metadata":
+        names = dataset.metadata.groupby('name').count()
+        names = names[names['age'] > args.unique_name_count]
+        names = names.index.get_level_values(0)
+        idx = [dataset.metadata['name'] == name for name in names]
+        result_idx = [False] * len(dataset.metadata)
+        for i in idx:
+          result_idx = np.logical_or(result_idx, i)
+          
+        return dataset.metadata.loc[result_idx].sort_values(by=['name', 'age'])
   elif args.dataset == "cacd":
     np.random.seed(1000)
-    return dataset.metadata.sample(args.no_of_samples).reset_index()
+    if args.source_type == 'file':
+      filenames = pd.read_csv(args.drift_source_filename)
+      idx = [dataset.metadata['filename'] == filename for filename in filenames['filename']]
+      result_idx = [False]*len(dataset.metadata)
+      for i in idx:
+          result_idx = np.logical_or(result_idx, i)
+    
+      return dataset.metadata.loc[result_idx].reset_index()
+    elif args.source_type == 'not_in_file':
+      filenames = pd.read_csv(args.drift_source_filename)
+      idx = [dataset.metadata['filename'] == filename for filename in filenames['filename']]
+      result_idx = [False]*len(dataset.metadata)
+      for i in idx:
+          result_idx = np.logical_or(result_idx, i)
+      
+      return dataset.metadata.loc[np.logical_not(result_idx)].reset_index()
+    elif args.source_type == "ordered_metadata":
+      names = dataset.metadata.groupby('name').count()
+      names = names[names['age'] > args.unique_name_count]
+      names = names.index.get_level_values(0)
+      idx = [dataset.metadata['name'] == name for name in names]
+      result_idx = [False] * len(dataset.metadata)
+      for i in idx:
+        result_idx = np.logical_or(result_idx, 1)
+        
+      return dataset.metadata.loc[result_idx].sort_values(by=['name', 'age'])
 
 def load_dataset(args, whylogs, input_shape=(-1,160,160,3)):
   dataset = None
@@ -95,7 +151,7 @@ def load_dataset(args, whylogs, input_shape=(-1,160,160,3)):
                               dim=(input_shape[1],input_shape[2]), batch_size=args.batch_size)
   elif args.dataset == "fgnet":
     augmentation_generator = get_augmented_datasets()
-    dataset = FGNETDataset(whylogs, args.metadata, list_IDs=None, 
+    dataset = FGNETDataset(whylogs, args.metadata, list_IDs=list(range(args.no_of_samples)), 
                            color_mode='rgb', augmentation_generator=augmentation_generator, data_dir=args.data_dir, 
                            dim=(input_shape[1],input_shape[2]), batch_size=args.batch_size)
   
@@ -115,28 +171,40 @@ if __name__ == "__main__":
   dataset.set_metadata(
     get_reduced_metadata(args, dataset)
   )
-
+  
+  args.no_of_samples = len(dataset.metadata)
+  
   iterator = dataset.get_iterator_face_classificaton(args.colormode, args.batch_size, args.data_dir, augmentation_generator, x_col='filename', y_cols=['name'])
   
   # setup experiment
   experiment = FaceNetWithoutAgingExperiment(dataset, logger=whylogs, model_loader=model_loader)
   
+  model_loader_detect = None
+  if args.model_detect == 'YuNet_onnx':
+    model_loader_detect = YuNetModelLoader(whylogs, args.model_path_detect, input_shape=[1,args.input_shape[1],args.input_shape[2],args.input_shape[3]], conf_threshold=args.conf_threshold, nms_threshold=args.nms_threshold, backend=args.backend, target=args.target, top_k=args.top_k)
+  
+  detectorExperiment = FaceClassificationByImages(dataset, whylogs, model_loader_detect)
+  
   """
   Collect Data
   """
-  embeddings = experiment.collect_data(args.data_collection_pkl, args.data_dir, args.batch_size, iterator=iterator, model=args.model)
+  embeddings = experiment.collect_data(args.data_collection_pkl, iterator=iterator, model=args.model, detectorExperiment=detectorExperiment)
+  
+  # if os.path.isfile(args.data_collection_pkl):
+  #   embeddings = tf.concat(pickle.load(open("../data_collection/detection_agedb_inferences_baseline_cvae_14k.pkl", "rb")), axis=0)
 
-  result_euclidean_distances, result_cosine_similarities = experiment.calculate_face_distance(embeddings)
+  result_euclidean_distances, result_cosine_similarities = experiment.calculate_face_distance(embeddings.numpy())
+  # result_euclidean_distances, result_cosine_similarities = experiment.calculate_face_mahalanonis_distance(embeddings.numpy())
 
   # if not os.path.isfile(args.data_collection_pkl):
   #   data_collection_pkl = Path(args.data_collection_pkl)
-  #
+  
   #   with open(data_collection_pkl, 'wb') as data_collection_file:
   #     pickle.dump(embeddings, data_collection_file)
       
   with mlflow.start_run(experiment_id=args.experiment_id):
     
-    if args.datset == 'agedb':
+    if args.dataset == 'agedb':
       counter_labels = Counter(dataset.metadata['name'])
     elif args.dataset == 'cacd':
       counter_labels = Counter(dataset.metadata['identity'])
@@ -150,22 +218,29 @@ if __name__ == "__main__":
     y_labels = [1]*result_euclidean_distances[0, :counter_labels[name_first]].flatten().shape[0] + \
       [0]*result_euclidean_distances[0, counter_labels[name_first]:].flatten().shape[0]
 
+    count = counter_labels[name_first]
     for ii, (identity, label) in tqdm(enumerate(counter_labels.items())):
         if ii == 0:
             continue
-        array = np.append(array, result_euclidean_distances[ii, :counter_labels[identity]].flatten())
-        y_labels += [1]*result_euclidean_distances[ii, :counter_labels[identity]].flatten().shape[0]
-        array = np.append(array, result_euclidean_distances[ii, counter_labels[identity]:].flatten())
-        y_labels += [0]*result_euclidean_distances[ii, counter_labels[identity]:].flatten().shape[0]
+        array = np.append(array, result_euclidean_distances[ii, count:count+counter_labels[identity]].flatten())
+        y_labels += [1]*result_euclidean_distances[ii, count:count+counter_labels[identity]].flatten().shape[0]
+        array = np.append(array, result_euclidean_distances[ii, count+counter_labels[identity]:].flatten())
+        array = np.append(array, result_euclidean_distances[ii, :count].flatten())
+        y_labels += [0]*result_euclidean_distances[ii, count+counter_labels[identity]:].flatten().shape[0]
+        y_labels += [0]*result_euclidean_distances[ii, :count].flatten().shape[0]
+        count += counter_labels[identity]
         
     array_cos = np.append(result_cosine_similarities[0, :counter_labels[name_first]].flatten(), \
       result_cosine_similarities[0, counter_labels[name_first]:].flatten())
 
+    count = counter_labels[name_first]
     for ii, (identity, label) in tqdm(enumerate(counter_labels.items())):
         if ii == 0:
             continue
-        array_cos = np.append(array_cos, result_cosine_similarities[ii, :counter_labels[identity]].flatten())
-        array_cos = np.append(array_cos, result_cosine_similarities[ii, counter_labels[identity]:].flatten())
+        array_cos = np.append(array_cos, result_cosine_similarities[ii, count:count+counter_labels[identity]].flatten())
+        array_cos = np.append(array_cos, result_cosine_similarities[ii, count+counter_labels[identity]:].flatten())
+        array_cos = np.append(array_cos, result_cosine_similarities[ii, :count].flatten())
+        count += counter_labels[identity]
 
     array_full = np.concatenate([array.reshape(-1, 1), array_cos.reshape(-1, 1)], axis=1)
 
@@ -174,7 +249,15 @@ if __name__ == "__main__":
                 label='Non-similar Images')
     plt.scatter(array_full[np.array(y_labels) == 1, 0], array_full[np.array(y_labels) == 1, 1], color='orange',
                 label='Similar Images')
-    plt.xlabel("Euclidean Distance", fontsize=16)
+    plt.xlabel("Euclidean Distances", fontsize=16)
     plt.ylabel("Cosine Similarity", fontsize=16)
     plt.legend()
-    plt.savefig("scatter_plot.png")
+    mlflow.log_figure(fig, "scatter_plot_baseline_euclidean.png")
+    
+    fig = plt.figure(figsize=(12, 8))
+    plt.hist(array_full[np.array(y_labels) == 0, 0], label='Non-similar Images', color='red')
+    plt.hist(array_full[np.array(y_labels) == 1, 0], label='Similar Images', color='blue')
+    plt.legend()
+    plt.xlabel("Euclidean Distances", fontsize=16)
+    plt.ylabel("Count", fontsize=16)
+    mlflow.log_figure(fig, "hist_plot_baseline_euclidean.png")
